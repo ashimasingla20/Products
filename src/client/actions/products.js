9e75f0f7356b4914a81115e897251970c3666cfa
Ashima Singla <ashima_singla20@yahoo.com>
2020-02-26 08:46:26 +0530

    cleanup

--- src/client/actions/products.js
+++ src/client/actions/products.js
@@ -1,25 +1,25 @@
 import axios from 'axios';
 import { FETCH_PRODUCTS_REQUEST,
-  FETCH_PRODUCTS_SUCCESS,
-  FETCH_PRODUCTS_FAILURE,
-  RESET_PRODUCTS
+	FETCH_PRODUCTS_SUCCESS,
+	FETCH_PRODUCTS_FAILURE,
+	RESET_PRODUCTS
 } from './constants';
 export const fetchAllProducts = (page) => async(dispatch) => {
-  dispatch({type: FETCH_PRODUCTS_REQUEST})
-  try {
-    const res = await axios.get(`https://node-sample-api.herokuapp.com/api/products?page=${page}`)
-    dispatch({
-      type: FETCH_PRODUCTS_SUCCESS,
-      payload: res.data
-    })
-  } catch(error) {
-    dispatch({
-      type: FETCH_PRODUCTS_FAILURE,
-      payload: error
-    })
-  }
+	dispatch({type: FETCH_PRODUCTS_REQUEST})
+	try {
+		const res = await axios.get(`https://node-sample-api.herokuapp.com/api/products?page=${page}`)
+		dispatch({
+			type: FETCH_PRODUCTS_SUCCESS,
+			payload: res.data
+		})
+	} catch(error) {
+		dispatch({
+			type: FETCH_PRODUCTS_FAILURE,
+			payload: error
+		})
+	}
 }
 export const resetFetchAllProducts = () => async(dispatch) => {
-  dispatch({type: RESET_PRODUCTS})
+	dispatch({type: RESET_PRODUCTS})
 }
 
