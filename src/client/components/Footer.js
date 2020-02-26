6df480079da903b97c1bb6f97477ad74329e29e6
Ashima Singla <ashima_singla20@yahoo.com>
2020-02-26 08:21:41 +0530

    add

--- src/client/components/Footer.js
+++ src/client/components/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/withStyles'
-import s from '../styles/Footer.scss';
+import footerStyle from '../styles/Footer.scss';
 const Footer = () => {
-  return (<footer className={s.footer}>
-    <div className={s.text}>
-      <span>Made with ❤️ in India</span>
-    </div>
-  </footer>)
+	return (<footer className={footerStyle.footer}>
+		<div className={s.text}>
+			<span>Made with ❤️ in India</span>
+		</div>
+	</footer>)
 }
-export default withStyles(s)(Footer);
+export default withStyles(footerStyle)(Footer);
