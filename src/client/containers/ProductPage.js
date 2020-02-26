225bce659fec39aa7bbbac0fd007b253eaa748d3
Ashima Singla <ashima_singla20@yahoo.com>
2020-02-26 08:10:22 +0530

    indented

--- src/client/containers/ProductPage.js
+++ src/client/containers/ProductPage.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
-import { useParams } from "react-router-dom";
 import { fetchProductById,resetFetchProduct } from '../actions/product';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -14,93 +13,86 @@ import useSSR from 'use-ssr';
 const Rating = ({rating}) => {
   const totalStars = 5;
   return (<div className={ProductStyles.stars}>
-    {[...Array(totalStars)].map((n, i) => ( 
-        i <rating 
-          ? <IoIosStar
-            size={25}
-            key={`star-${i}`}/>
-          : <IoMdStarOutline 
-            size={25}
-            key={`star-${i}`}/> 
-      ))}
+	{[...Array(totalStars)].map((ele, i) => ( 
+		i <rating 
+		? <IoIosStar
+			size={25}
+			key={`star-${i}`}/>
+		: <IoMdStarOutline 
+			size={25}
+			key={`star-${i}`}/> 
+	))}
   </div>)
 }
 function ProductPage(props) {
-  const { match: { params: {id} } } = props;
-  const [product, setProduct] = useState(props.product);
-  let { isBrowser, isServer } = useSSR();
-  if(isServer){
-    useEffect(() => {
-      console.log('here running use effect')
-      setProduct(props.staticContext.product);
-      return () => props.resetFetchProduct()
-    },[props.staticContext.product])
-  }
-  if(isBrowser) {
-    setTimeout(() =>{
-      const browserData = window.INITIAL_STATE;
-      setProduct(browserData.product);
-    },0);
-  }
-  useEffect(() => {
-    if(!product.productInfo 
-      || (product.productInfo && id!= product.productInfo.id)) {
-      props.fetchProductById(id)
-    }
-  },[])
-  const { productInfo } = product;
-  if(!product || !productInfo) return null;
-  const setRedirect = () => {
-    props.history.push(`/products`);
-  }
-  return (
-    <div>
-    <Header/>
-      <div className={ProductStyles.container}>
-        <div className={ProductStyles.imagebox}>
-          <a href="/products" className={ProductStyles.back}>
-            <FaArrowLeft size={25} className={ProductStyles.icon}/>
-          </a>
-          {/* <span className={ProductStyles.back} onClick={setRedirect}>
-            <FaArrowLeft size={25} className={ProductStyles.icon}/>
-          </span> */}
-          <ErrorBoundary>
-            <img 
-              className={ProductStyles.image}
-              src={productInfo.img} 
-              alt={productInfo.name}/>
-          </ErrorBoundary>
-        </div>
-        <ErrorBoundary>
-          <div>
-            <div className={ProductStyles.block}>
-              <p className={ProductStyles.head}>Product Info</p>
-              <p className={ProductStyles.value}>{productInfo.name} </p>
-            </div>
-            <div className={ProductStyles.block}>
-              <p className={ProductStyles.head}>Rating</p>
-              <div className={ProductStyles.ratingcontainer}>
-                <Rating rating={productInfo.rating}/>
-              </div>
-            </div>
-            <div className={ProductStyles.block}>
-              <p className={ProductStyles.head}>Description</p>
-              <div className={classNames(ProductStyles.value, ProductStyles.description)} >
-                {productInfo.description}
-              </div>
-            </div>
-          </div>
-        </ErrorBoundary>
-      </div>
-    <Footer/>
-  </div>
-  )
+	const { match: { params: {id} } } = props;
+  	const [product, setProduct] = useState(props.product);
+  	let { isBrowser, isServer } = useSSR();
+  	if(isServer){
+		useEffect(() => {
+			console.log('here running use effect')
+			setProduct(props.staticContext.product);
+			return () => props.resetFetchProduct()
+		},[props.staticContext.product])
+  	}
+	if(isBrowser) {
+		useEffect(() => {
+			const browserData = window.INITIAL_STATE;
+			setProduct(browserData.product);
+		},[]);
+	}
+	useEffect(() => {
+		if(!product.productInfo 
+		|| (product.productInfo && id!= product.productInfo.id)) {
+		props.fetchProductById(id)
+		}
+	},[])
+	const { productInfo } = product;
+	if(!product || !productInfo) return null;
+	return (<div>
+		<Header/>
+		<div className={ProductStyles.container}>
+			<div className={ProductStyles.imagebox}>
+				<a href="/products" className={ProductStyles.back}>
+					<FaArrowLeft size={25} className={ProductStyles.icon}/>
+				</a>
+				<ErrorBoundary>
+					<img 
+					className={ProductStyles.image}
+					src={productInfo.img} 
+					alt={productInfo.name}/>
+				</ErrorBoundary>
+			</div>
+			<ErrorBoundary>
+				<div>
+					<div className={ProductStyles.block}>
+						<p className={ProductStyles.head}>Product Info</p>
+						<p className={ProductStyles.value}>{productInfo.name} </p>
+					</div>
+					<div className={ProductStyles.block}>
+						<p className={ProductStyles.head}>Rating</p>
+						<div className={ProductStyles.ratingcontainer}>
+							<Rating rating={productInfo.rating}/>
+						</div>
+					</div>
+					<div className={ProductStyles.block}>
+						<p className={ProductStyles.head}>Description</p>
+						<div className={classNames(ProductStyles.value, ProductStyles.description)} >
+							{productInfo.description}
+						</div>
+					</div>
+				</div>
+			</ErrorBoundary>
+		</div>
+		<Footer/>
+	</div>
+	)
 }
 function mapStateToProps(state) {
-  return { product: state.product }
+	return { product: state.product }
 }
 function loadData(store, id='') {
-  return store.dispatch(fetchProductById(id))
+	return store.dispatch(fetchProductById(id))
 }
 export default {
   loadData,
--- src/client/containers/ProductsListPage.js
+++ src/client/containers/ProductsListPage.js
@@ -10,84 +10,84 @@ import ProductElements from '../components/ProductList/ProductElements';
 import Carousel from '../components/ProductList/Carousel';
 import useSSR from 'use-ssr';
 function ProductsList(props) {
-  const [products, setProducts] = useState(props.products);
-  const [carousel, setCarousel] = useState(props.carousel);
-  let { isBrowser, isServer } = useSSR();
-  if(isBrowser) {
-    useEffect(() => {
-      const browserData = window.INITIAL_STATE;
-      setProducts(browserData.products);
-      setCarousel(browserData.carousel);
-    },[]);
-  }
-  if(isServer){
-    useEffect(() => {
-      setProducts(props.staticContext.products);
-      setCarousel(props.staticContext.carousel);
-      return () => {
-        props.resetCarousel()
-        props.resetFetchAllProducts()
-      }
-    },[])
-  }
-  //fallback for ssr only fetch if not ssr data available
-  useEffect(() => {
-    if(!products.data.length) {
-      props.fetchAllProducts(1);
-    }
-    if(!carousel.data || (!carousel.data.carousel && !carousel.data.carousel.length)) {
-      props.fetchCarousel();
-    }
-  },[]);
-  useEffect(() => {
-    setProducts(props.products)
-  },[props.products])
-  useEffect(() => {
-    setCarousel(props.carousel)
-  },[props.carousel])
-  const getMoreProducts = (e, page) => {
-    props.fetchAllProducts(page);
-  }
-  console.log(products);
-  return (
-    <div>
-      <Header/>
-      {!!carousel.data
-        && carousel.data.carousel
-        && <Carousel carouselData={carousel.data.carousel}/> }
-      {!!products 
-        && <ul className={ProductListStyle.container}>
-        <ProductElements products ={products.data}/>
-      </ul>}
-      <button 
-        type="button" 
-        className={ProductListStyle.button}
-        onClick={(e) => getMoreProducts(e, products.page)}>
-        Load More
-      </button>
-      <Footer/>
-    </div>
-  )
+	const [products, setProducts] = useState(props.products);
+	const [carousel, setCarousel] = useState(props.carousel);
+	let { isBrowser, isServer } = useSSR();
+	if(isBrowser) {
+		useEffect(() => {
+			const browserData = window.INITIAL_STATE;
+			setProducts(browserData.products);
+			setCarousel(browserData.carousel);
+		},[]);
+	}
+	if(isServer){
+		useEffect(() => {
+		setProducts(props.staticContext.products);
+		setCarousel(props.staticContext.carousel);
+		return () => {
+			props.resetCarousel()
+			props.resetFetchAllProducts()
+		}
+		},[])
+	}
+	//fallback for ssr only fetch if not ssr data available
+	useEffect(() => {
+		if(!products.data.length) {
+			props.fetchAllProducts(1);
+		}
+		if(!carousel.data || (!carousel.data.carousel && !carousel.data.carousel.length)) {
+			props.fetchCarousel();
+		}
+	},[]);
+	useEffect(() => {
+		setProducts(props.products)
+	},[props.products])
+	useEffect(() => {
+		setCarousel(props.carousel)
+	},[props.carousel])
+	const getMoreProducts = (e, page) => {
+		props.fetchAllProducts(page);
+	}
+	return (
+		<div>
+		<Header/>
+		{!!carousel.data
+			&& carousel.data.carousel
+			&& <Carousel carouselData={carousel.data.carousel}/> }
+		{!!products 
+			&& <ul className={ProductListStyle.container}>
+			<ProductElements products ={products.data}/>
+		</ul>}
+		<button 
+			type="button" 
+			className={ProductListStyle.button}
+			onClick={(e) => getMoreProducts(e, products.page)}>
+			<span className={ProductListStyle.text}>Load More</span>
+		</button>
+		<Footer/>
+		</div>
+	)
 }
 function mapStateToProps(state) {
-  return { 
-    products: state.products,
-    carousel: state.carousel
-  }
+	return { 
+		products: state.products,
+		carousel: state.carousel
+	}
 }
 function loadData(store, id='') {
-  return Promise.all([
-    store.dispatch(fetchAllProducts(1)),
-    store.dispatch(fetchCarousel())
-  ]);
+	return Promise.all([
+		store.dispatch(fetchAllProducts(1)),
+		store.dispatch(fetchCarousel())
+	]);
 }
 
 export default {
   loadData,
   component: withStyles(ProductListStyle)(connect(mapStateToProps, 
-      {fetchAllProducts, 
-        fetchCarousel,
-        resetFetchAllProducts,
-        resetCarousel
-      })(ProductsList))
+	{
+		fetchAllProducts, 
+		fetchCarousel,
+		resetFetchAllProducts,
+		resetCarousel
+	})(ProductsList))
 }
