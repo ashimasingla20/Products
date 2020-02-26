e129b9c651da9cc6700d18276800c4941b43b8ed
Ashima Singla <ashima_singla20@yahoo.com>
2020-02-26 09:01:09 +0530

    indent-fx

--- webpack.base.js
+++ webpack.base.js
@@ -5,62 +5,62 @@ module.exports = {
   // Tell webpack to run babel on every file it runs through
   mode: 'development',
   module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            'react',
-            'stage-0',//for async
-            ['env', { targets: { browsers: ['last 2 versions'] } }]
-          ]
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'isomorphic-style-loader',
-          {
-            loader: require.resolve('css-loader'),
-            options: {
-              importLoaders: 1,
-            },
-          },
-          {
-            loader: require.resolve('postcss-loader'),
-            options: {
-              ident: 'postcss',
-              plugins: () => [
-                require('postcss-flexbugs-fixes'),
-                autoprefixer({
-                  browsers: [
-                    '>1%',
-                    'last 4 versions',
-                    'Firefox ESR',
-                    'not ie < 9', // React doesn't support IE8 anyway
-                  ],
-                  flexbox: 'no-2009',
-                }),
-              ],
-            },
-          },
-        ],
-      },
-      {
-        test: /\.scss$/,
-        loaders: [
-            'isomorphic-style-loader',
-            {
-              loader: require.resolve('css-loader'),
-              options: {
-                modules: true,
-              },
-            },
-            require.resolve('sass-loader')
-        ]
-      }
-    ]
+	rules: [
+		{
+			test: /\.js?$/,
+			loader: 'babel-loader',
+			exclude: /node_modules/,
+			options: {
+				presets: [
+					'react',
+					'stage-0',//for async
+					['env', { targets: { browsers: ['last 2 versions'] } }]
+				]
+			}
+		},
+		{
+			test: /\.css$/,
+			use: [
+				'isomorphic-style-loader',
+				{
+					loader: require.resolve('css-loader'),
+					options: {
+						importLoaders: 1,
+					},
+				},
+				{
+					loader: require.resolve('postcss-loader'),
+					options: {
+						ident: 'postcss',
+						plugins: () => [
+							require('postcss-flexbugs-fixes'),
+							autoprefixer({
+								browsers: [
+								'>1%',
+								'last 4 versions',
+								'Firefox ESR',
+								'not ie < 9', // React doesn't support IE8 anyway
+								],
+								flexbox: 'no-2009',
+							}),
+						],
+					},
+				},
+			],
+		},
+		{
+		test: /\.scss$/,
+			loaders: [
+				'isomorphic-style-loader',
+				{
+					loader: require.resolve('css-loader'),
+					options: {
+						modules: true,
+					},
+				},
+				require.resolve('sass-loader')
+			]
+		}
+	]
   }
 };
