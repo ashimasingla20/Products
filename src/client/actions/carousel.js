89f0fa739568ec83234c1537380f682884052800
Ashima Singla <ashima_singla20@yahoo.com>
2020-02-26 08:44:19 +0530

    add

--- src/client/actions/carousel.js
+++ src/client/actions/carousel.js
@@ -1,24 +1,24 @@
 import axios from 'axios';
 import { FETCH_CAROUSEL_REQUEST,
-  FETCH_CAROUSEL_SUCCESS,
-  FETCH_CAROUSEL_FAILURE,
-  RESET_CAROUSEL
+	FETCH_CAROUSEL_SUCCESS,
+	FETCH_CAROUSEL_FAILURE,
+	RESET_CAROUSEL
 } from './constants';
 export const fetchCarousel = () => async(dispatch, id) => {
-  dispatch({type: FETCH_CAROUSEL_REQUEST})
-  try {
-    const res = await axios.get(`https://node-sample-api.herokuapp.com/api/home`);
-    dispatch({
-      type: FETCH_CAROUSEL_SUCCESS,
-      payload: res.data
-    })
-  } catch(error) {
-    dispatch({
-      type: FETCH_CAROUSEL_FAILURE,
-      payload: error
-    })
-  }
+	dispatch({type: FETCH_CAROUSEL_REQUEST})
+	try {
+		const res = await axios.get(`https://node-sample-api.herokuapp.com/api/home`);
+		dispatch({
+			type: FETCH_CAROUSEL_SUCCESS,
+			payload: res.data
+		})
+	} catch(error) {
+		dispatch({
+			type: FETCH_CAROUSEL_FAILURE,
+			payload: error
+		})
+	}
 }
 export const resetCarousel = () => async(dispatch) => {
-  dispatch({type: RESET_CAROUSEL})
+	dispatch({type: RESET_CAROUSEL})
 }
